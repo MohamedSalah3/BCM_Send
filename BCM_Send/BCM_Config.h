29698fe7555ce1430f641548b6936e10a96aba47
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-03-05 02:15:33 +0200

    typedef of init fun

--- BCM_Send/BCM_Config.h
+++ BCM_Send/BCM_Config.h
@@ -14,6 +14,23 @@
 #include "timers.h"
 #include "led.h"
 
+/*u8_channel_Protcol*/
+#define UART  0
+#deine  SPI  1
+#define I2C 2
+/*u8_BCM_ID*/
+#define TASK1_ID A
+#define TASK2_ID  B
+#define TASK3_ID  C
+
+/* u8_data_Lenght*/
+
+
+typedef ST_BCM_ConfigType{
+uint8_t u8_channel_Protcol;
+uint8_t u8_BCM_ID;
+uint8_t u8_data_Lenght;
+}BCM_ConfigType;
 
 
 #endif /* BCM_CONFIG_H_ */
